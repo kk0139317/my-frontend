4c8e6c76fddde7321e9d5d78dda1b58e8a1c0ec0
kk0139317 <kk4881757@gmail.com>
2024-05-22 11:09:53 +0530

    Changes to be committed: 	modified:   app/page.tsx 	modified:   next.config.mjs 	modified:   package-lock.json 	modified:   package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    async rewrites() {
+      return [
+        {
+          source: '/detect',
+          destination: 'http://localhost:8000/detect',
+        },
+      ];
+    },
+  };
+  
